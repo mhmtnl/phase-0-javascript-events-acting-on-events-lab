fc116b0884be01d68ad8bd980a238ca3d45b67bc
mehmetunlu <munlu4037@gmail.com>
2023-04-22 23:29:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "50%";
+dodger.style.justifyContent = "50%"
+
+
+// moveDodgerLeft()
+// moveDodgerRight()
+
+
+function moveDodgerLeft() {
+    const position = dodger.style.left;
+    const left = parseInt(position, 10);
+    if (left > 0) { dodger.style.left = `${left - 5}px`; }
+}
+
+function moveDodgerRight() {
+    const position = dodger.style.left;
+    const right = parseInt(position, 10);
+    if (right > 0) { dodger.style.left = `${right +  5}px`;}
+}
+
+
+document.addEventListener("keydown", function (e) { if (e.key === "ArrowLeft") { moveDodgerLeft(); } });
+document.addEventListener("keydown", function (e) { if (e.key === "ArrowRight") { moveDodgerRight(); } });
